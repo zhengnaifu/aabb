406694a2096bf14ba96c0d391c56eda55b759a86
znf <123@163.com>
2018-01-14 16:57:05 +0800

    haha

---
+++ src/main/java/com/example/demo/FormServlet.java
@@ -0,0 +1,42 @@
+package com.example.demo;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FormServlet
+ */
+public class FormServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FormServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		String str_a = request.getParameter("a");
+		String str_b = request.getParameter("b");
+		int sum = Integer.parseInt(str_a) + Integer.parseInt(str_b);
+		response.getWriter().append(String.valueOf(sum));
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
